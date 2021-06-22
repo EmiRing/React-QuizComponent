d0deb7eb97e8de09485b37e2c6650b136e564799
EmiRing <emil.ringwall@gmail.com>
2021-06-22 09:25:36 +0200

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,11 +9,9 @@ class Quiz extends Component {
   }
   render(){
     return (
-      <div>
-        <div className="quizQuestion">
+      <div><div className="QuizQuestion">
             {quizData.quiz_questions[0].instruction_text}
-        </div>
-      </div>
+        </div></div>
     )
   }
 }
